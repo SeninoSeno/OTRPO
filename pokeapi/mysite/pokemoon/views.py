c8c206f346b3263e1c4173355e52d9679cc6c511
SeninoSeno <arseniy2002@mail.ru>
2023-10-15 22:27:55 +0500

    add fight page

--- pokeapi/mysite/pokemoon/views.py
+++ pokeapi/mysite/pokemoon/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render
 import requests
 from django.core.paginator import Paginator
@@ -7,6 +9,7 @@ url = "https://pokeapi.co/api/v2/pokemon"
 request_res = requests.get(f'{url}').json()
 pokemon_count = requests.get(f'{url}').json()['count']
 pokemons = requests.get(f'{url}?limit={pokemon_count}&offset=0').json()['results']
+
 
 def index(request):
     q = request.GET.get('q')
@@ -35,13 +38,28 @@ def index(request):
 
 def pokemon(request, name):
     info = get_all_info(name)
-    print(info)
     context = {
         "pokemon": info,
         "hp": info['stats'][0]['base_stat'],
         "attack": info['stats'][1]['base_stat'],
     }
     return render(request, "pokemon.html", context)
+
+def fight(request, name):
+    pc_id = random.randint(1, pokemon_count)
+    pc_info = get_all_info(pc_id)
+    user_info = get_all_info(name)
+
+    context = {
+        "pc": pc_info,
+        "pc_hp": pc_info['stats'][0]['base_stat'],
+        "pc_attack": pc_info['stats'][1]['base_stat'],
+
+        "user": user_info,
+        "user_hp": user_info['stats'][0]['base_stat'],
+        "user_attack": user_info['stats'][1]['base_stat'],
+    }
+    return render(request, "fight.html", context)
 
 def get_some_info(name):
     return requests
